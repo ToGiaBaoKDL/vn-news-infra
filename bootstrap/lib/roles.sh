25dbd8e8477a913148b54eb8f534ee41620c1660
Tô Gia Bảo <baokdl2226@gmail.com>
2026-06-04 22:24:20 +0700

    refactor(infra): own airflow runtime deployment
    Build the Airflow provider image from infra and load orchestration through GitDagBundle with env-driven endpoints.

--- bootstrap/lib/roles.sh
+++ bootstrap/lib/roles.sh
@@ -10,8 +10,11 @@ configure_control_role() {
 
   ensure_dir /srv/vn-news-control 0775 root:vn-news
   ensure_dir /srv/vn-news-control/airflow-db 0775 root:vn-news
+  ensure_dir /srv/vn-news-control/airflow-dag-bundles 0775 root:vn-news
   ensure_dir /srv/vn-news-control/airflow-logs 0775 root:vn-news
+  chown -R "$airflow_uid:$airflow_gid" /srv/vn-news-control/airflow-dag-bundles
   chown -R "$airflow_uid:$airflow_gid" /srv/vn-news-control/airflow-logs
+  chmod 0770 /srv/vn-news-control/airflow-dag-bundles
   chmod 0770 /srv/vn-news-control/airflow-logs
   ensure_dir /srv/vn-news-control/prometheus 0775 root:vn-news
 }
