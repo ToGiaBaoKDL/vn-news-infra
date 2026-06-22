3a046a517e28c99f7a8e36256b753f75b1e8ae84
Tô Gia Bảo <baokdl2226@gmail.com>
2026-06-22 12:04:26 +0700

    Make Spark runtime dirs deploy-user accessible

--- bootstrap/lib/roles.sh
+++ bootstrap/lib/roles.sh
@@ -6,16 +6,16 @@ configure_data_role() {
 
 configure_control_role() {
   local airflow_uid="${VN_NEWS_AIRFLOW_UID:-50000}"
-  local airflow_gid="${VN_NEWS_AIRFLOW_GID:-0}"
+  local host_group="${VN_NEWS_HOST_SHARED_GROUP:-vn-news}"
 
   ensure_dir /srv/vn-news-control 0775 root:vn-news
   ensure_dir /srv/vn-news-control/airflow-db 0775 root:vn-news
   ensure_dir /srv/vn-news-control/airflow-dag-bundles 0775 root:vn-news
   ensure_dir /srv/vn-news-control/airflow-logs 0775 root:vn-news
-  ensure_dir /srv/vn-news-control/spark 0770 "$airflow_uid:$airflow_gid"
-  ensure_dir /srv/vn-news-control/spark/checkpoints 0770 "$airflow_uid:$airflow_gid"
-  chown -R "$airflow_uid:$airflow_gid" /srv/vn-news-control/airflow-dag-bundles
-  chown -R "$airflow_uid:$airflow_gid" /srv/vn-news-control/airflow-logs
+  ensure_dir /srv/vn-news-control/spark 0770 "$airflow_uid:$host_group"
+  ensure_dir /srv/vn-news-control/spark/checkpoints 0770 "$airflow_uid:$host_group"
+  chown -R "$airflow_uid:$host_group" /srv/vn-news-control/airflow-dag-bundles
+  chown -R "$airflow_uid:$host_group" /srv/vn-news-control/airflow-logs
   chmod 0770 /srv/vn-news-control/airflow-dag-bundles
   chmod 0770 /srv/vn-news-control/airflow-logs
   ensure_dir /srv/vn-news-control/prometheus 0775 root:vn-news
@@ -23,10 +23,10 @@ configure_control_role() {
 
 configure_processing_role() {
   local spark_uid="${VN_NEWS_SPARK_UID:-185}"
-  local spark_gid="${VN_NEWS_SPARK_GID:-0}"
+  local host_group="${VN_NEWS_HOST_SHARED_GROUP:-vn-news}"
 
   ensure_dir /srv/vn-news-processing 0775 root:vn-news
-  ensure_dir /srv/vn-news-processing/spark 0770 "$spark_uid:$spark_gid"
-  ensure_dir /srv/vn-news-processing/spark/local 0770 "$spark_uid:$spark_gid"
-  ensure_dir /srv/vn-news-processing/spark/work 0770 "$spark_uid:$spark_gid"
+  ensure_dir /srv/vn-news-processing/spark 0770 "$spark_uid:$host_group"
+  ensure_dir /srv/vn-news-processing/spark/local 0770 "$spark_uid:$host_group"
+  ensure_dir /srv/vn-news-processing/spark/work 0770 "$spark_uid:$host_group"
 }
